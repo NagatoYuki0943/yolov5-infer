d6ff5dab8bfbdc2d489d4bec763e03cf935e7108
yuki0943 <lihongtu12138@outlook.com>
2022-10-09 16:56:26 +0800

    onnxruntime运行成功，opencv失败

--- python/opencvrun.py
+++ python/opencvrun.py
@@ -1,11 +1,9 @@
-"""onnx图片需要调整BGR2RGB, 并调整通道为[B, C, H, W], 且需要归一化
+"""onnx图片需要调整为[B, C, H, W],且需要归一化
 """
-
 
 from pathlib import Path
 
 import onnx
-import onnxruntime as ort
 
 import numpy as np
 import cv2
@@ -58,17 +56,22 @@ def get_image(image_path):
         Tuple: 原图, 输入的tensor, 填充的宽, 填充的高
     """
     img = cv2.imread(str(Path(image_path)))
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # BGR2RGB
 
-    img_reized, delta_w ,delta_h = resize_and_pad(img_rgb, (640, 640))
+    img_reized, delta_w ,delta_h = resize_and_pad(img, (640, 640))
 
-    img_reized = img_reized.astype(np.float32)
-    img_reized /= 255.0                             # 归一化
+    # [H, W, C] -> [B, C, H, W] & BRG2RGB & 归一化等操作
+    # 当同时进行swapRB,scalefactor,mean,size操作时，优先按swapRB交换通道，其次按scalefactor比例缩放，然后按mean求减，最后按size进行resize操作
+    blob = cv2.dnn.blobFromImage(img_reized,
+                                swapRB=True,                    # 交换 Red 和 Blue 通道, BGR2RGB
+                                scalefactor=1.0 / 255,          # 图像各通道数值的缩放比例
+                                # mean=[0.485, 0.456, 0.406],   # 用于各通道减去的均值
+                                # std=[0.229, 0.224, 0.225],    # 没有std，不要自己加
+                                size=(640, 640),                # 图片大小 w,h
+                                crop=False,                     # 图像裁剪,默认为False.当值为True时，先按比例缩放，然后从中心裁剪成size尺寸
+                                ddepth=cv2.CV_32F               # 数据类型,可选 CV_32F 或者 CV_8U
+                                )
 
-    img_reized = img_reized.transpose(2, 0, 1)      # [H, W, C] -> [C, H, W]
-    input_tensor = np.expand_dims(img_reized, 0)    # [C, H, W] -> [B, C, H, W]
-
-    return img, input_tensor, delta_w ,delta_h
+    return img, blob, delta_w ,delta_h
 
 
 def check_onnx(onnx_path):
@@ -84,40 +87,17 @@ def check_onnx(onnx_path):
         print("Model correct")
 
 
-def get_onnx_model(onnx_path, cuda=False):
+def get_dnn_model(onnx_path):
     """获取模型
 
     Args:
         onnx_path (str): 模型路径
-        cuda (bool, optional): 是否使用cuda. Defaults to False.
 
     Returns:
-        InferenceSession: 推理模型
+        _type_: _description_
     """
     check_onnx(onnx_path)
-    so = ort.SessionOptions()
-    so.log_severity_level = 3
-    # 新版本onnxruntime-gpu这样指定cpu和gpu
-    if cuda:
-        model = ort.InferenceSession(onnx_path, sess_options=so, providers=['CUDAExecutionProvider'])
-    else:
-        model = ort.InferenceSession(onnx_path, sess_options=so, providers=['CPUExecutionProvider'])
-
-    #--------------------------------#
-    #   查看model中的内容
-    #   get_inputs()返回对象，[0]返回名字
-    #--------------------------------#
-    # print("model outputs: \n", model.get_inputs())    # 列表 [<onnxruntime.capi.onnxruntime_pybind11_state.NodeArg object at 0x000001B16D601730>]
-    # print(model.get_inputs()[0])                      # NodeArg(name='images', type='tensor(float)', shape=[1, 3, 640, 640])
-    # print(model.get_inputs()[0].name)                 # images
-    # print(model.get_inputs()[0].type)                 # tensor(float)
-    # print(model.get_inputs()[0].shape, "\n")          # [1, 3, 640, 640]
-
-    # print("model outputs: \n", model.get_outputs())   # 列表 [<onnxruntime.capi.onnxruntime_pybind11_state.NodeArg object at 0x000001B16D6016B0>, <onnxruntime.capi.onnxruntime_pybind11_state.NodeArg object at 0x000001B16D6017B0>, <onnxruntime.capi.onnxruntime_pybind11_state.NodeArg object at 0x000001B16D6017F0>, <onnxruntime.capi.onnxruntime_pybind11_state.NodeArg object at 0x000001B16D601830>]
-    # print(model.get_outputs()[0])                     # NodeArg(name='output', type='tensor(float)', shape=[1, 25200, 85])
-    # print(model.get_outputs()[0].name)                # output
-    # print(model.get_outputs()[0].type)                # tensor(float)
-    # print(model.get_outputs()[0].shape, "\n")         # [1, 25200, 85]
+    model = cv2.dnn.readNetFromONNX(onnx_path)
 
     return model
 
@@ -190,14 +170,19 @@ def post(detections, delta_w ,delta_h, img):
 #--------------------------------#
 def inference():
     # 获取图片,扩展的宽高
-    img, input_tensor, delta_w ,delta_h = get_image(IMAGE_PATH)
+    img, blob, delta_w ,delta_h = get_image(IMAGE_PATH)
 
     # 获取模型
-    model = get_onnx_model(ONNX_PATH, False)
+    model = get_dnn_model(ONNX_PATH)
 
     start = time.time()
-    detections = model.run(None, {model.get_inputs()[0].name: input_tensor})
-    # print(detections[0].shape)                        # [1, 25200, 85]
+    # 设置模型输入
+    model.setInput(blob)
+    # 推理模型结果
+    detections = model.forward()
+    print(detections[0].shape)                        # [1, 25200, 85]
+    return
+
     detections = np.squeeze(detections[0])              # [25200, 85]
 
     # Step 8. Postprocessing including NMS
